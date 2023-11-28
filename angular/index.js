426077e9e6b11f6ca23c8b556cc38af69c395051
Davide Ficano <davide.ficano@gmail.com>
2023-11-28 13:14:29 +0100

    added missing dependency

--- angular/index.js
+++ angular/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   "extends" : [
-    "plugin:jsdoc/recommended",
-    "plugin:import/recommended",
     "plugin:@angular-eslint/recommended",
     "plugin:@angular-eslint/template/process-inline-templates",
     "./eslint-angular.json"
