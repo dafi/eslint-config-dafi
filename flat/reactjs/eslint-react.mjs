072ca02c77c698543b871eff8dadcf3568de0c56
Davide Ficano <davide.ficano@gmail.com>
2024-03-27 07:43:49 +0100

    added jsx-no-useless-fragment

--- flat/reactjs/eslint-react.mjs
+++ flat/reactjs/eslint-react.mjs
@@ -1,11 +1,10 @@
 export default {
   rules: {
-    "react/display-name": "off",
     "react/destructuring-assignment": "error",
+    "react/display-name": "off",
     "react/forbid-component-props": "off",
-    "react/sort-default-props": "off",
-    "react/no-array-index-key": "error",
-    "react/no-danger": "error",
+    "react/hook-use-state": "error",
+    "react/jsx-boolean-value": "error",
     "react/jsx-filename-extension": [
       "error",
       {
@@ -15,21 +14,28 @@ export default {
         ]
       }
     ],
-    "react/self-closing-comp": "error",
-    "react/jsx-no-bind": "off",
     "react/jsx-max-depth": [
       "error",
       {
         "max": 6
       }
     ],
+    "react/jsx-no-bind": "off",
     "react/jsx-no-literals": "off",
-    "react/jsx-boolean-value": "error",
+    "react/jsx-no-useless-fragment": [
+      "error",
+      {
+        "allowExpressions": true
+      }
+    ],
+    "react/jsx-uses-react": "off",
+    "react/no-array-index-key": "error",
+    "react/no-danger": "error",
     "react/no-multi-comp": "error",
     "react/prefer-read-only-props": "error",
-    "react/hook-use-state": "error",
-    "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
-    "react/require-default-props": "error"
+    "react/require-default-props": "error",
+    "react/self-closing-comp": "error",
+    "react/sort-default-props": "off"
   }
 };
